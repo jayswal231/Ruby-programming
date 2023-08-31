1ddd4dd885df07c12d7fed0ac041aac0dbc67292
jayswal231 <mukeshjayswal62@gmail.com>
2023-08-31 20:49:07 +0545

    update

--- core-ruby/ruby_start.rb
+++ core-ruby/ruby_start.rb
@@ -63,22 +63,81 @@
 
   
 #----class----
-class Car
-    def initialize(s,f)
-        @speed, @fuel = s,f
-    end 
+# class Car
+#     def initialize(s,f)   
+#         @speed, @fuel = s,f
+#     end 
 
-    def speed
-        puts "find speed details"
-        return @speed
-    end
+#     def speed
+#         puts "find speed details"
+#         return @speed
+#     end
     
-    def fuel
-        puts "check fuel"
-        return @fuel
+#     def fuel
+#         puts "check fuel"
+#         return @fuel
+#     end
+# end
+
+# obj1 = Car.new(10,70)
+# puts obj1.speed
+# puts obj1.fuel
+
+
+# class Home
+#     #setter
+#     attr_writer:name,:age
+#     #getter
+#     attr_reader:name,:age
+
+#     def to_s
+#         return "the name and age: #{name}, #{age}"
+#     end
+# end
+# o=Home.new
+# o.name="mukesh"
+# o.age="23"
+
+# puts o.name
+# puts o.age
+# puts o
+
+#-----inheritance-----
+# class College
+#     attr_accessor:name,:place  #setter and getter both in one
+#     def initialize(name,place)
+#         @name=name
+#         @place=place
+#     end
+# end
+
+# class Student < College
+#     attr_accessor:faculty
+#     def details
+#         return "welcome"
+#     end
+# end
+
+# st = Student.new("himalaya","lalitpur")
+# puts st.inspect
+# puts st.details
+
+# ---freeze---
+class Data
+    attr_accessor:name,:age
+    def initialize(n,a)
+        @name=n
+        @age=a
     end
 end
 
-obj1 = Car.new(10,70)
-puts obj1.speed
-puts obj1.fuel
+dt= Data.new("mukesh",23)
+puts dt.name
+puts dt.age
+
+dt.freeze
+if (dt.frozen?)
+    puts "data is frozen"
+else
+    puts "data is not frozen"
+end
